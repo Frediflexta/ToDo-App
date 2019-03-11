0ff99e3f561672f4b306583ce8fed6fa201abe08
frediflexta <fredrick.adegoke@gmail.com>
2019-03-11 22:31:05 +0100

    create a new todo and store it in the db

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
+    <div class="form-group col-lg-6 col-lg-offset-6">
+      <form action="/create/todo" method="post">
+        @csrf
+        <input class="form-control form-control-lg" type="text" name="todo" placeholder="Create new todo">
+      </form>
+     </div>
+
   <h5>
     @foreach($todos as $todo)
 
